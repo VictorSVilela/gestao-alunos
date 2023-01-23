148da58c9fe03c6bf127543b64a19f0c995ab97e
Victor Vilela <dev.victorvilela@gmail.com>
2023-01-23 17:02:34 -0300

    Initial commit

---
+++ backend/src/test/java/com/vilela/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilela.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
